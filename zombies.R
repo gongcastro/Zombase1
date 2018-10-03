de73b0e5af6bbd787f42c4e98e0fb1ed3dfb4075
gongcastro <gonzalo.garciadecastro01@estudiant.upf.edu>
2018-10-03 13:00:09 +0200

    New films, continent variable, some budgets and box, and .csv file

--- zombies.R
+++ zombies.R
@@ -2,15 +2,14 @@ library(tidyverse)
 library(magrittr)
 library(car)
 
+# import data
 zombies <-
   read.delim("zombies.txt",
              sep = "\t",
              na.strings = c("NA", ""),
              header = TRUE,
              dec = ".",
-             colClasses = c("character", "factor", "numeric", "character", "factor"))
-
-countsCountry <- zombies %$% country %>% table %>% as.vector
+             colClasses = c("character", "factor", "numeric", "character", "factor", "factor"))
 
 zombies %>%
   ggplot(aes(x = year,
@@ -26,6 +25,7 @@ zombies %>%
   theme_minimal()
 
 zombies %>%
+  filter(year > 1975) %>%
   ggplot(aes(x = year,
              y = factor(country,
                         levels = rev(levels(country))))) +
@@ -39,7 +39,15 @@ zombies %>%
   theme_minimal()
 
 zombies %>%
-  ggplot(aes(x = year, y = country, fill = type)) +
-  geom_tile()
+  filter(year > 1975) %>%
+  ggplot(aes(x = year, y = country, color = type)) +
+  geom_count(alpha = 0.5) +
+  theme_minimal()
+
+zombies %>%
+  ggplot(aes(x = type, y = country, color = type)) +
+  geom_count() +
+  theme_minimal()
+
 
   
