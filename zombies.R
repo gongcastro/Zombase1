529e0e35c8e095723615fd955c413adfb1eeffec
gongcastro <gonzalo.garciadecastro@upf.edu>
2019-02-10 15:22:39 +0100

    I added new data, tidied the code, and added density by type and region and frequency by type and region plots.

--- zombies.R
+++ zombies.R
@@ -1,32 +1,61 @@
-# zombies
+# zombies: Production of zombie material over the years
 # Gonzalo García-Castro, gonzaloggc95@gmail.com
 
-##################################################
+# ------------------------------------------------------------------------------
+
+
+# set up -----------------------------------------------------------------------
 
 # load packages
-library(magrittr) # for pipes
-library(readxl)   # for importing Excel files
-library(dplyr)    # for wrangling data
-library(tibble)
-library(ggplot2)  # for visualising data
-library(viridis)  # for plot colours
-library(ggthemes)
-library(plotly)
-library(extrafont)
-library(gridExtra)
+library(magrittr)  # for pipes
+library(readxl)    # for importing Excel files
+library(dplyr)     # for wrangling data
+library(ggplot2)   # for visualising data
+library(viridis)   # for plot colours
+library(extrafont) # for text fonts
+library(gridExtra) # for arranging plots together
 
-library(ggmap)
-library(maptools)
-library(maps)
-library(googleway)
-library(placement)
+# extra -------------------------------------------------------------------
 
-# import data #################################################################
-data <- read_xlsx("~/projects/zombies/zombies.xlsx", sheet = "zombies")
+# group countries by region
+europe <- c("portugal", "spain", "france", "andorra", "italy", "greece", "belgium", "ireland",
+            "uk", "netherlands", "germany", "austria", "switzerland", "czech_republic", "hungary",
+            "poland", "norway", "sweden", "denmark", "finland", "iceland", "san_marino", "liechtenstein",
+            "albania", "serbia", "bosnia_herzegovin", "montenegro", "croatia", "slovenia", "slovakia",
+            "romania", "bulgary", "ukraine", "belarus", "stonia", "latvia", "lithuania", "macedonia",
+            "malta", "cyprus", "luxembourg", "monaco", "vatican_city", "moldova")
+america <- c("canada", "usa", "mexico", "guatemala", "honduras", "panama", "el_salvador", "cuba",
+             "haiti", "dominican_republic", "puerto_rico", "venezuela", "colombia", "peru", "ecuador",
+             "bolivia", "chile")
 
-# country against year ########################################################
+
+# import data -------------------------------------------------------------------
+data <-
+  read_xlsx("~/projects/zombies/zombies.xlsx", sheet = "zombies") %>%
+  mutate(region = case_when(country = ))
+
+# overal distribution by year ---------------------------------------------------
+ggplot(data, aes(year)) +
+  geom_density(aes(colour = country)) +
+  scale_colour_viridis(discrete = TRUE, option = "inferno") +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "black",
+                                        colour = "white",
+                                        size = 0.5),
+        legend.position = "bottom",
+        plot.background = element_rect(fill = "black"),
+        text = element_text(colour = "white", family = "Arial Black"),
+        axis.text.x = element_text(colour = "white", family = "Arial"),
+        axis.text.y = element_text(colour = "white", family = "Arial"),
+        legend.background = element_rect(fill = "black")) +
+  ggsave("~/projects/zombies/year.png" , width = 15)
+
+
+
+# country against year -----------------------------------------------------------
 country_year <-
-  data %>% filter(country != "NA", year != "NA") %>% arrange(country) %>%
+  data %>%
+  filter(country != "NA", year != "NA") %>% arrange(country) %>%
   ggplot(., aes(year, country)) +
   geom_point(aes(color = type), show.legend = TRUE, alpha = 0.5) +
   labs(title = "Zombie-related material over the years by country",
@@ -45,9 +74,10 @@ country_year <-
         legend.background = element_rect(fill = "black")) +
   ggsave("~/projects/zombies/country_year.png", height = , width = 10)
 
-# country counts ################################################################
+# country counts -----------------------------------------------------------------
 country_counts <-
-  data %>% filter(country != "NA", year != "NA") %>% group_by(country, type) %>%
+  data %>%
+  filter(country != "NA", year != "NA") %>% group_by(country, type) %>%
   ggplot(., aes(x = as.factor(country), fill = type)) +
   geom_bar(color = "white", size = 0.1) +
   labs(title = "Zombie-related material by country",
@@ -71,12 +101,12 @@ country_counts <-
         legend.background = element_rect(fill = "black")) +
   ggsave("~/projects/zombies/country_counts.png", height = 7, width = 10)
 
-# arrange plots ##################################################################
+# arrange plots -----------------------------------------------------------------
 grid.arrange(country_year, country_counts, nrow = 1) 
 
 
 
-# map ############################################################################
+# map ---------------------------------------------------------------------------
 map <- map_data("world") %>% as.tibble()
 counts <- data %>% filter(country != "NA", year != "NA") %>% count(country) %>% select(region = country)
 ggplot(map, aes(long, lat, group = group, fill = region)) +
